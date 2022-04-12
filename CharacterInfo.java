21b9ca324474015262f242d056c8820a0a8a8404
amadayami <amadayamichael@gmail.com>
2022-04-12 15:29:19 -0600

    Added setters and getters for most of the character info vars. Still need to add for the last 5. But don't know if I want to make a String array

--- CharacterInfo.java
+++ CharacterInfo.java
@@ -17,7 +17,7 @@ public class CharacterInfo {
 
     public CharacterInfo(String name, String classT, String background,
                          String race, String alignment, String playerName,
-                         int speed){
+                         int speed, int hpMax){
         setCharName(name);
         setCharClass(classT);
         setBackground(background);
@@ -25,6 +25,7 @@ public class CharacterInfo {
         setAlignment(alignment);
         setPlayerName(playerName);
         setSpeed(speed);
+        setHpMax(hpMax);
     }
 
     public CharacterInfo(){
@@ -35,8 +36,12 @@ public class CharacterInfo {
         setAlignment("Lawful Good");
         setPlayerName("Amelia Martin");
         setSpeed(30);
+        setHpMax(10);
     }
-    
+
+    /*
+    Setters for the character info variables
+     */
     public void setCharName(String newName) {
         this.charName = newName;
     }
@@ -62,4 +67,57 @@ public class CharacterInfo {
     public void setSpeed(int newSpeed) {
         this.speed = newSpeed;
     }
+
+    public void setArmorClass(int newAC) { this.armorClass = newAC; }
+
+    public void setHpMax(int maxHealth) { this.hpMax = maxHealth; }
+
+    public void setHpCur(int curHealth) { this.hpCur = curHealth; }
+
+    public void setHpTemp(int tempHealth) { this.hpTemp = tempHealth; }
+
+    public void setCp(int cp) { this.cp = cp; }
+
+    public void setSp(int sp) { this.sp = sp; }
+
+    public void setEp(int ep) { this.ep = ep; }
+
+    public void setGp(int gp) { this.gp = gp; }
+
+    public void setPp(int pp) { this.pp = pp; }
+
+    /*
+    Getters for the character info variables
+     */
+    public String getCharName() { return this.charName; }
+
+    public String getCharClass() { return this.charClass; }
+
+    public String getBackground() { return this.background; }
+
+    public String getRace() { return this.race; }
+
+    public String getAlignment() { return this.alignment; }
+
+    public String getPlayerName() { return this.playerName; }
+
+    public int getSpeed() { return this.speed; }
+
+    public int getArmorClass() { return this.armorClass; }
+
+    public int getHpMax() { return this.hpMax; }
+
+    public int getHpCur() { return this.hpCur; }
+
+    public int getHpTemp() { return this.hpTemp; }
+
+    public int getCp() { return this.cp; }
+
+    public int getSp() { return this.sp; }
+
+    public int getEp() { return this.ep; }
+
+    public int getGp() { return this.gp; }
+
+    public int getPp() { return this.pp; }
 }
